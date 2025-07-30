f41e6c6b4e8c8aa457217a584e3b86f851df9d2f
edinstance <edinstance@gmail.com>
2025-07-30 18:53:47 +0100

    fix: updated comparisons

--- tests/functions/transactions/process_transactions/test_sqs.py
+++ tests/functions/transactions/process_transactions/test_sqs.py
@@ -1,6 +1,8 @@
 import pytest
 
-from functions.transactions.process_transactions.process_transactions.sqs import format_sqs_message
+from functions.transactions.process_transactions.process_transactions.sqs import (
+    format_sqs_message,
+)
 
 
 class TestSqsHelpers:
@@ -10,5 +12,5 @@ class TestSqsHelpers:
         with pytest.raises(ValueError) as exception_info:
             format_sqs_message("", "")
 
-            assert exception_info.type == ValueError
+            assert exception_info.type is ValueError
             assert exception_info.value.args[0] == "Record must be a dictionary"
